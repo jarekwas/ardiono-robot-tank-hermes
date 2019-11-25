013511aab9df9e1d32916fd740fdb3ba071904db
Jarek Wasowski <jarek@truesoftware.pl>
2019-11-25 21:40:17 +0100

    [WIP] Walki z poprawnym skrecaniem

--- App/App.ino
+++ App/App.ino
@@ -15,28 +15,30 @@ char command;
 int speedL = 0;
 int speedR = 0;
 int velocity = 0;
-int turn=0;
+int turn = 0;
+float turnFloat = 0;
 bool runing = false;
 
 void stopIfFault()
 {
-  if (md.getM1Fault())
-  {
-    Serial.println("M1 fault");
-    while(1);
-  }
-  if (md.getM2Fault())
-  {
-    Serial.println("M2 fault");
-    while(1);
-  }
+    if (md.getM1Fault())
+    {
+        Serial.println("M1 fault");
+        while (1)
+            ;
+    }
+    if (md.getM2Fault())
+    {
+        Serial.println("M2 fault");
+        while (1)
+            ;
+    }
 }
-
 
 void loop()
 {
- stopIfFault();
-  
+    stopIfFault();
+
     if (bluetooth_serial.available())
     {
         String s = "";
@@ -46,86 +48,116 @@ void loop()
             s += c;
             delay(5);
         }
-        Serial.print(s);
         command = s[0];
 
         switch (command)
         {
         case 'p':
-            speedR = parseIntFromCmd(s)-400;
+            speedR = parseIntFromCmd(s) - 400;
             break;
         case 'l':
-            speedL = parseIntFromCmd(s)-400;
+            speedL = parseIntFromCmd(s) - 400;
             break;
         case 'g':
             runing = !runing;
+
+            Serial.print("GO!");
+            Serial.print(", r: ");
+            Serial.print(speedR);
+            Serial.print(", l: ");
+            Serial.print(speedL);
+            Serial.print(", v: ");
+            Serial.println(velocity);
+
             break;
         case 's':
             runing = !runing;
             break;
-       case 'v':
-            velocity = parseIntFromCmd(s)-400;
+        case 'v':
+            velocity = parseIntFromCmd(s) - 400;
             speedL = velocity;
             speedR = velocity;
-            Serial.println("Ustawiono prędkość"+velocity);
-
+            Serial.print("Velocity:");
+            Serial.print(", r: ");
+            Serial.print(speedR);
+            Serial.print(", l: ");
+            Serial.print(speedL);
+            Serial.print(", v: ");
+            Serial.println(velocity);
             break;
         case 't':
             turn = parseIntFromCmd(s) - 400;
-            if(turn>0) {
-              speedL = velocity;
-              speedL =speedL-turn;
-              speedR = velocity;
-            Serial.print("Skrecam wprawo");
-            Serial.print("r: "+speedL);
-            Serial.print("v: "+velocity);
-            Serial.println("Skręca SpeedL"+speedL);
+
+            if (turn > 0)
+            {
+                speedL = velocity;
+                speedR = velocity;
+
+                turnFloat = (400 - abs(turn)) / 400;
+                speedL = speedL * turnFloat;
+                Serial.print("t > 0 | turn: ");
+                Serial.print(turn);
+                Serial.print(", r: ");
+                Serial.print(speedR);
+                Serial.print(", turnFloat: ");
+                Serial.print(turnFloat);
+                Serial.print(", l: ");
+                Serial.print(speedL);
+                Serial.print(", v: ");
+                Serial.println(velocity);
             }
-            else if(turn<0){
+            else if (turn < 0)
+            {
 
-            speedR = velocity;
-            speedR =speedR+turn;
-            speedL = velocity;
-            Serial.println("Skręcam wlewo");
-            Serial.println("Skręca SpeedR"+speedR);
-            Serial.println("Ustawiono prędkość"+velocity);
-            Serial.println("Skręca SpeedL"+speedL);
-      
+                speedR = velocity;
+                speedL = velocity;
 
-            
-            } else if(turn=0){
-              Serial.print("M2 current: ");
-            speedR = velocity;
-            speedL = velocity;
+                turnFloat = (400 - abs(turn)) / 400;
+                speedR = speedR * turnFloat;
+
+                Serial.print("t > 0 | turn: ");
+                Serial.print(turn);
+                Serial.print(", r: ");
+                Serial.print(speedR);
+                Serial.print(", turnFloat: ");
+                Serial.print(turnFloat);
+                Serial.print(", l: ");
+                Serial.print(speedL);
+                Serial.print(", v: ");
+                Serial.println(velocity);
+            }
+            else if (turn == 0)
+            {
+                speedR = velocity;
+                speedL = velocity;
+
+                Serial.print("t == 0");
+                Serial.print(", r: ");
+                Serial.print(speedR);
+                Serial.print(", l: ");
+                Serial.print(speedL);
+                Serial.print(", v: ");
+                Serial.println(velocity);
             }
 
             break;
-
-            
-
-            
         }
     }
 
     if (runing)
     {
         md.setM2Speed(speedR);
-        md.setM1Speed(speedL*-1);
-               delay(2);
-               
+        md.setM1Speed(speedL * -1);
+        delay(2);
 
-    /*    Serial.print("M2 current: ");
+        /*    Serial.print("M2 current: ");
         Serial.println(md.getM2CurrentMilliamps());
         Serial.print("M1 current: ");
         Serial.println(md.getM1CurrentMilliamps());
         delay(2); */
-
-
-
-        
     }
     else
-        {
+    {
         md.setM1Speed(0);
         md.setM2Speed(0);
     }
